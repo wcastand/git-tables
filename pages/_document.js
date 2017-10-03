e8da40368c6b0276cf5df90c90adfdf310d457a4
wcastand <wcastand@gmail.com>
2017-10-03 11:58:26 +0200

    fix error jsx

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,16 @@ const css = `
 * {box-sizing: border-box;}
 body{ font-family:'Roboto', sans-serif; font-size: 16px; margin: 0; padding: 0; background-color: #F1F1F1;}
 `
+const script = `
+if(process.browser){
+  window.dataLayer = window.dataLayer || []
+  function gtag(){
+    dataLayer.push(arguments)
+  }
+  gtag('js', new Date())
+  gtag('config', 'UA-107418384-1')
+}
+`
 
 export default class MyDocument extends Document {
   static async getInitialProps({renderPage}) {
@@ -25,17 +35,15 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107418384-1" />
+          <script>{script}</script>
+
           <title>Git Tables</title>
           <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
           <link rel="stylesheet" href="/static/app.css" />
           <style>{css}</style>
           <style dangerouslySetInnerHTML={{__html: this.props.css}} />
-          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107418384-1" />
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments)};
-            gtag('js', new Date()); gtag('config', 'UA-107418384-1');
-          </script>
         </Head>
         <body>
           <Main />
